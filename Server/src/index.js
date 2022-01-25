6a2edd351b90f5d2ac23bac3e0e74b67034610c9
VaibhavJadhav2703 <43025108+VaibhavJadhav-27@users.noreply.github.com>
2022-01-25 20:18:48 +0530

    created index.js file and done with basic setup

---
+++ Server/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express();
+const cors = require('cors')
+
+//Settings
+app.set('port', process.env.PORT || 4000);
+
+//Middlewares
+app.use(express.json());
+app.use(cors());
+
+//Starting the server
+app.listen(app.get('port'), () => {
+    console.log('Server is running on port ', app.get('port'));
+})
