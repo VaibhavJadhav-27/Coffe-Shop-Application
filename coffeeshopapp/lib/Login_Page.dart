676ed220d03b2e49caa63db30901f325347e8127
VaibhavJadhav2703 <43025108+VaibhavJadhav-27@users.noreply.github.com>
2022-03-12 21:09:09 +0530

    login page created and main dart file updated

---
+++ coffeeshopapp/lib/Login_Page.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: file_names, camel_case_types
+
+import 'package:flutter/material.dart';
+
+class Login_Page extends StatefulWidget {
+  const Login_Page({Key? key}) : super(key: key);
+
+  @override
+  State<Login_Page> createState() => _Login_PageState();
+}
+
+class _Login_PageState extends State<Login_Page> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
