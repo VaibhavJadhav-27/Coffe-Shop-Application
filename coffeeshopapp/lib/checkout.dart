73151d28cd60be2f4decd6160d7a0679b56d53db
VaibhavJadhav2703 <43025108+VaibhavJadhav-27@users.noreply.github.com>
2022-03-29 00:18:37 +0530

    halfy done

--- coffeeshopapp/lib/checkout.dart
+++ coffeeshopapp/lib/checkout.dart
@@ -1,6 +1,11 @@
-// ignore_for_file: camel_case_types, prefer_const_constructors, duplicate_ignore, prefer_const_constructors_in_immutables, prefer_final_fields
+// ignore_for_file: camel_case_types, prefer_const_constructors, duplicate_ignore, prefer_const_constructors_in_immutables, prefer_final_fields, prefer_const_literals_to_create_immutables
+
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'cartclass.dart';
 
 class checkout extends StatefulWidget {
   final String profile;
@@ -16,15 +21,68 @@ class _checkoutState extends State<checkout> {
   late TextEditingController _address = TextEditingController();
   late TextEditingController _pincode = TextEditingController();
   late TextEditingController _phoneno = TextEditingController();
-  late TextEditingController _city = TextEditingController();
+
+  late int custid;
+
   @override
   Widget build(BuildContext context) {
+    String custname = widget.profile;
+    int total = widget.totalprice;
+    int pricewithcharges = total  + 50;
     // ignore: prefer_const_constructors
+    void getcartitems() async {
+      //for getting customer id from customer name
+      var url =
+          Uri.parse('http://192.168.0.103:4000/customer/customer/$custname');
+      var response = await http.get(url);
+      var custjson = json.decode(response.body);
+      print(custjson);
+      custid = custjson[0]["custid"];
+      print(custid);
+
+      var url1 = Uri.parse('http://192.168.0.103:4000/cart/cart/$custid');
+      var response1 = await http.get(url1);
+      var cartjson = json.decode(response1.body);
+      print(cartjson);
+      List<Cart> cartitems = [];
+      var items = "";
+      var listitems = [];
+      for (var u in cartjson) {
+        items =
+            "Item Name : " + u["itemname"]; ", Quantity: " + u["itemquantity"].toString();
+        listitems.add(items);
+      }
+      print(listitems);
+
+      var url2 = Uri.parse('http://192.168.0.103:4000/orderdb/order');
+      Map<String, String> requestHeaders = {
+        'Content-type': 'application/json',
+        'Accept': 'application/json',
+      };
+      var ordertime = DateTime.now();
+      var body = jsonEncode({
+        'custid': custid,
+        'items' : listitems,
+        'totalamount' : total,
+        'payment_type': 'cod',
+        'ordertime' : ordertime,
+        'delivery_type': 'cod',
+        'isreceived' : 'false'
+      });
+      var response2 = await http.post(url2,headers: requestHeaders,body : body);
+      
+      if (response2.statusCode == 200) {
+        print("Response status = ${response1.statusCode}");
+        print("Response body : " + response2.body.toString());
+      }
+    }
+
     return SafeArea(
         // ignore: prefer_const_constructors
         child: Scaffold(
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
@@ -37,10 +95,10 @@ class _checkoutState extends State<checkout> {
                       icon: Icon(
                         Icons.arrow_back_ios,
                         color: Colors.black,
-                        size: 15,
+                        size: 25,
                       )),
                   SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.3,
+                    width: MediaQuery.of(context).size.width * 0.20,
                   ),
                   Text("Checkout",
                       style: TextStyle(
@@ -55,48 +113,58 @@ class _checkoutState extends State<checkout> {
               height: 40,
             ),
             Padding(
-              padding: const EdgeInsets.only(left: 10),
-              child: Text("Delivery Address :"),
+              padding: const EdgeInsets.only(left: 20),
+              child: Text(
+                "Delivery Address :",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+              ),
             ),
             Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.only(left: 20, bottom: 20),
               child: SizedBox(
                 child: TextField(
                   controller: _address,
+                  maxLines: 4,
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     hintText: "Enter Address",
                   ),
                 ),
-                height: 80,
-                width: MediaQuery.of(context).size.width * 0.7,
+                height: 100,
+                width: MediaQuery.of(context).size.width * 0.8,
               ),
             ),
             Padding(
-              padding: const EdgeInsets.only(left: 10),
-              child: Text("Pincode :"),
+              padding: const EdgeInsets.only(left: 20),
+              child: Text(
+                "Pincode :",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+              ),
             ),
             Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.only(left: 20, bottom: 20),
               child: SizedBox(
                 child: TextField(
                   controller: _pincode,
                   keyboardType: TextInputType.number,
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
-                    hintText: "Enter Pincode",
+                    hintText: "Enter Pin code",
                   ),
                 ),
-                height: 40,
-                width: MediaQuery.of(context).size.width * 0.5,
+                height: 50,
+                width: MediaQuery.of(context).size.width * 0.8,
               ),
             ),
             Padding(
-              padding: const EdgeInsets.only(left: 10),
-              child: Text("Phone Number :"),
+              padding: const EdgeInsets.only(left: 20),
+              child: Text(
+                "Phone Number :",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+              ),
             ),
             Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.only(left: 20, bottom: 20),
               child: SizedBox(
                 child: TextField(
                   controller: _phoneno,
@@ -105,56 +173,102 @@ class _checkoutState extends State<checkout> {
                     hintText: "Enter Phone Number",
                   ),
                 ),
-                height: 40,
-                width: MediaQuery.of(context).size.width * 0.5,
+                height: 50,
+                width: MediaQuery.of(context).size.width * 0.8,
               ),
             ),
             Padding(
-              padding: const EdgeInsets.only(left: 10),
-              child: Text("City :"),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SizedBox(
-                child: TextField(
-                  controller: _city,
-                  readOnly: true,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: "Mumbai",
+              padding: const EdgeInsets.only(left: 20),
+              child: Row(
+                children: [
+                  Text(
+                    "City :",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
                   ),
-                ),
-                height: 80,
-                width: MediaQuery.of(context).size.width * 0.7,
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text(
+                    "Mumbai",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+                  ),
+                ],
               ),
             ),
             SizedBox(
-              height: 50,
+              height: 30,
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [Text("Sub Total :"), Text("")],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [Text("Delivery Charges :"), Text("")],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [Text(" Total :"), Text("")],
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
-                child: Text(
-                  "Place Order",
-                  style: TextStyle(fontSize: 20),
-                ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Divider(
+                thickness: 2,
+                color: Colors.black,
               ),
-              style: ElevatedButton.styleFrom(
-                  primary: Color.fromRGBO(21, 102, 59, 1)),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, bottom: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Sub Total :",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                  Text("${total}",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),)
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, bottom: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Delivery Charges :",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                  Text("50",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),)
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, bottom: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    " Total :",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                  Text("${pricewithcharges}",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),)
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  getcartitems();
+                },
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 60, vertical: 15),
+                  child: Text(
+                    "Place Order",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                    primary: Color.fromRGBO(21, 102, 59, 1)),
+              ),
             )
           ],
         ),
