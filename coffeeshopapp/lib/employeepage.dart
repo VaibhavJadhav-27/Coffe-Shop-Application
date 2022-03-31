996e9bd12792f1250ab21290812813e2037b1cfe
VaibhavJadhav2703 <43025108+VaibhavJadhav-27@users.noreply.github.com>
2022-03-31 22:54:33 +0530

    created new admin dashboard pages

--- coffeeshopapp/lib/employeepage.dart
+++ coffeeshopapp/lib/employeepage.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, prefer_final_fields, unused_field
 
 import 'dart:convert';
 
@@ -15,6 +15,18 @@ class EmployeePage extends StatefulWidget {
 
 class _EmployeePageState extends State<EmployeePage> {
   String name = "";
+  String email = "";
+  String password = "";
+  String phoneno = "";
+  String designation = "";
+  int salary = 0;
+  late TextEditingController _namecontroller = TextEditingController();
+  late TextEditingController _emailcontroller = TextEditingController();
+  late TextEditingController _passcontroller = TextEditingController();
+  late TextEditingController _designationcontroller = TextEditingController();
+  late TextEditingController _salarycontroller = TextEditingController();
+  late TextEditingController _phonenocontroller = TextEditingController();
+
   List<Employee> emprecords = [];
   Future<List<Employee>> fetchdetails() async {
     var url = Uri.parse('http://192.168.0.103:4000/employee/employee');
@@ -44,7 +56,41 @@ class _EmployeePageState extends State<EmployeePage> {
         child: SingleChildScrollView(
           scrollDirection: Axis.horizontal,
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+                child: Row(
+                  //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    IconButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: Icon(
+                          Icons.arrow_back_ios,
+                          color: Colors.black,
+                          size: 15,
+                        )),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.1,
+                    ),
+                    Text(
+                      "Employee Records",
+                      style: TextStyle(
+                          fontFamily: "Comfortaa",
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromRGBO(21, 102, 59, 1)),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
               DataTable(
                   columns: [
                     DataColumn(label: Text("Name")),
@@ -73,6 +119,62 @@ class _EmployeePageState extends State<EmployeePage> {
                 height: 30,
               ),
               Text(name),
+              SizedBox(
+                height: 40,
+                width: 130,
+                child: TextField(
+                  controller: _namecontroller,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Name",
+                  ),
+                  onChanged: (value) {},
+                ),
+              ),
+              SizedBox(
+                height: 40,
+                width: 130,
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Name",
+                  ),
+                  onChanged: (value) {},
+                ),
+              ),
+              SizedBox(
+                height: 40,
+                width: 130,
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Name",
+                  ),
+                  onChanged: (value) {},
+                ),
+              ),
+              SizedBox(
+                height: 40,
+                width: 130,
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Name",
+                  ),
+                  onChanged: (value) {},
+                ),
+              ),
+              SizedBox(
+                height: 40,
+                width: 130,
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Name",
+                  ),
+                  onChanged: (value) {},
+                ),
+              ),
             ],
           ),
         ),
